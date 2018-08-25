6f9258f3f45ca07d37008200935ae88690d9d552
geohope15 <georepersents@gmail.com>
2018-08-26 05:27:45 +0530

    add howitwork.html

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def partners():
     return render_template("partners.html")
 	
+@app.route("/howitwork")
+def howitwork():
+    return render_template("howitwork.html")
+	
 @app.route("/")
 def home():
     return render_template("index.html")
