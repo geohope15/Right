0ab8ba99b9e96c2175c93cb063104eaed9b7dc68
geohope15 <georepersents@gmail.com>
2018-08-25 22:15:28 +0530

    add

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask,render_template
 
-app=Flask(__name__)
+from flask import Flask , render_template, request, jsonify
 
-@app.route('/')
+app = Flask(__name__)
+
+
+	
+@app.route("/")
 def home():
-    return render_template('unitowin.html')
+    return render_template("index.html")
 
-if __name__=='__main__':
+@app.route("/unitowin")
+def unitowin():
+    return render_template("unitowin.html")
+
+@app.route("/wintouni")
+def wintouni():
+    return render_template("wintouni.html")
+
+@app.route("/wintozawgyi")
+def wintozawgyi():
+    return render_template("wintozawgyi.html")
+	
+@app.route("/zawgyitowin")
+def zawgyitowin():
+    return render_template("zawgyitowin.html")
+
+@app.route("/zawgyitouni")
+def zawgyitouni():
+    return render_template("zawgyitouni.html")
+
+@app.route("/unitozawgyi")
+def unitozawgyi():
+    return render_template("unitozawgyi.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
+
